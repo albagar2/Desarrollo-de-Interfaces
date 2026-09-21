cb7c3963e758c8539d0a2e846b23c612998263e9
albagar2 <baciapez@gmail.com>
2026-09-21 18:22:42 +0200

    Second example for unit two

--- UD1-EJ2-CLASE/Program.cs
+++ UD1-EJ2-CLASE/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace UD1_EJ2_CLASE
 {
+
+    // clase principal
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Worker worker1 = new Worker("Pedro");
+
+            worker1.SetName("Juan");
+
+            worker1.Name = "Juan";
+            worker1.Salary = 1500;
+
+            Console.WriteLine($"Nombre del trabajador: {worker1.Name} y su salario es {worker1.Salary}");
         }
     }
 }
---
+++ UD1-EJ2-CLASE/Worker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD1_EJ2_CLASE
+{
+    internal class Worker
+    {
+        // EN JAVA SE USA ASI
+        private string name;
+        
+        public string GetName()
+        {
+            return name;
+        }
+
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        //constructor
+       /* public WorkerPrueba(string name)
+        {
+            this.name = name;
+        }
+
+        */
+        // EN C# SE USA ASI
+        public string Name { get; set; }
+
+        private double _salary;
+        public double Salary
+        {
+            //get { return Salary; } 
+            get => _salary;
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine($"Valor negativo. Ponemos el salario a cero ");
+                    _salary = 0;
+
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+        public Worker(string name)
+        {
+            Name = name;
+        }
+    }
+}
